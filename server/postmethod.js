e8cf73ded53d46ed578ee64db93c85238436b8d4
zlouis <jiannchyao@gmail.com>
2016-12-17 12:34:59 -0500

    newly added likes and delete post and sort by likes

--- server/postmethod.js
+++ server/postmethod.js
@@ -9,5 +9,25 @@ Meteor.methods({
         email: Meteor.user().emails[0].address
       }
     });
+  },
+  likePost: function(postId){
+    userSignedIn = Meteor.user() || false;
+    if(userSignedIn){
+      Posts.update({_id: postId}, {$inc: {likes: 1} });
+    }
+  },
+  deletePost: function(postId){
+    var post = Posts.findOne({_id: postId }),
+    postUserId = post.user._id;
+    currentUserId = Meteor.userId();
+    if(postUserId === currentUserId) {
+      console.log("User deleted post.")
+      Posts.remove({_id: postId});
+    } else if(Roles.userIsInRole(Meteor.user(), ['admin'])){
+      console.log("Admin deleted post.")
+      Posts.remove({_id: postId});
+    } else {
+      console.log("Someone's trying to delete posts and shouldn't be.")
+    }
   }
 });
